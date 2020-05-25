ea6695de32de343c0a4e2fb9ecb9101956292ef0
sinmunsub <sinmun501@naver.com>
2020-05-25 11:29:51 +0900

    wwww

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
+import Customer from './components/Customer'
 import './App.css';
+
+const customers=[{
+  'id':1,
+  'image': 'https://placeimg.com/64/64/any/1',
+  'name':'홍길동', 
+  'birthday':'961222', 
+  'gender':'남자',
+  'job':'대학생'
+},
+{
+  'id':2,
+  'image': 'https://placeimg.com/64/64/any/2',
+  'name':'신문섭', 
+  'birthday':'890810', 
+  'gender':'남자',
+  'job':'대학생'
+},
+{
+  'id':3,
+  'image': 'https://placeimg.com/64/64/any/3',
+  'name':'신지영', 
+  'birthday':'940224', 
+  'gender':'여자',
+  'job':'대학생'
+}
+]
 
 class App extends Component{
   render(){
-    return(
-      <div className="gray-background">
-        <img src={logo} lat="logo"/>
-        <h2>Let's develop management system!</h2>
-      </div>
+    return( 
+    <div>
+     {
+       customers.map(c=>{
+       return(
+       <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
+       )
+       })
+     }
+     </div>
     )
 
   }
